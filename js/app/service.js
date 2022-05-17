3f0b67fa8d095f045da4b669cc48a0a675cfc337
Ying Li <ying.li@suezenvironnement.com>
2022-05-17 21:23:54 +0200

    update for 2022

--- js/app/service.js
+++ js/app/service.js
@@ -10,109 +10,162 @@ angular
             "nationalite": "Chinoise",
             "tel": "06.69.43.94.11",
             "email": "ying.li.paris@gmail.com",
-            "position": "Ingénieur développement",
+            "permis": "B2",
+            "position": "Dev Lead / Tech Lead",
             "formations": [
-                { "annee": 2011, "nom": "Diplomée en Master 2 à UTC", "description": "Etudes Master 2 dans la spécialité «Systèmes Intelligents pour les Transports »" },
-                { "annee": 2008, "nom": "Université de Technologie de Compiègne (UTC)", "description": "Etudes d’ingénieur en Génie Informatique, dans le cadre des conventions d’échange avec l'Université de Xi’an Jiaotong(XJTU)" },
+                { "annee": 2011, "nom": "Diplomée en Master 2 à UTC", "description": "Etudes Master 2 dans la spécialité «Systèmes Intelligents pour les Transports»" },
+                { "annee": 2008, "nom": "Université de Technologie de Compiègne (UTC)", "description": "Etudes d’ingénieur en Génie Informatique, dans le cadre des conventions d’échange avec l'Université de Xi’an Jiaotong (XJTU)" },
                 { "annee": 2006, "nom": "Université de Xi’an Jiaotong (XJTU)", "description": "Etudes en Génie Electrique" },
             ],
             "competences": {
                 "programmation": [
                     {
                         "nom": "C#",
-                        "frameworks": ["WCF", "EntityFramework", "WPF"]
+                        "frameworks": [".Net5", "AspNet", "REST API", "WCF", "EntityFramework", "WPF"]
                     },
                     {
-                        "nom": "TypeScript, AngularJs"
+                        "nom": "Angular2+, TypeScript, AngularJs"
                     },
                     {
                         "nom": "C++"
                     }
                 ],
                 "rdbms": ["SQL Server", "MySQL"],
-                "servers": ["IIS", "Windows server", "GeoServer"],
-                "outils": ["Visual Studio", "Microsoft Visio 2007", "TFS", "Moq", "StyleCop", "FxCop", "CodeContracts"],
+                "servers": ["Windows server", "GeoServer", "IIS"],
+                "outils": ["Visual Studio", "Visual Studio Code", "Azure DevOps", "Git", "TFS", "Moq", "StyleCop", "FxCop", "CodeContracts"],
                 "langues": [
                     { "nom": "Français", "niveau": "bilingue", "code": "French" },
                     { "nom": "Chinois", "niveau": "maternelle", "code": "Chinese" },
-                    { "nom": "Anglais", "niveau": "couramment", "code": "English" }
-                ]
+                    { "nom": "Anglais", "niveau": "courant", "code": "English" }
+                ],
+                "permis": "B2",
+                "méthodologies": "ScrumAgile"
             },
             "interets": [
-                "Natation", "Danses de salon", "Couture", "Jeux de plateaux"
+                "Natation", "Badminton", "Danses de salon"
             ]
         };
 
 
         var _projets = [
             {
-                "nom": "SUEZ (Ondeo Systems)",
+                "nom": "SUEZ Smart Solutions",
                 "debut": "2011-02-01",
-                "description": "Au sein d'un service d'édition logicielle proposant des solutions temps réel pour la surveillance et le pilotage des réseaux de distribution et d'assainissement de l'eau, je suis intervenue sur les projets suivants :",
+                "description": "Au sein d'un service d'édition logicielle proposant des solutions sur le cycle de l'eau, je suis intervenue sur les projets suivants :",
                 "hiddentechs": "",
                 "projets": [
                     {
-                        "nom": "Aquadvanced",
-                        "debut": "2013-06-01",
-                        "description": "Aquadvanced est une solution temps réel de suivi et d'optimisation du fonctionnement des réseaux d'eau potable. La solution est composée par :",
+                        "nom": "Aquadvanced Production & Transport",
+                        "debut": "2019-04-01",
+                        "description": "Aquadvanced P&T est un logiciel qui optimise la consommation d’énergie pendant la production et le transport de l’eau potable. Il est composé de :",
                         "desc": [
-                            "Un back office qui permet la collecte des données, ainsi que les calculs d'indicateurs et la détection de fuite à l'aide de modèles statistiques et hydrauliques",
+                            "Un back office qui permet de collecter des données, calculer des indicateurs ainsi que de prévoir les commandes à envoyer afin de piloter les automates du réseau à l'aide de modèles d’optimisations et de modèles hydrauliques",
+                            "Un front office qui offre une vision cartographique des ouvrages, des indicateurs, ainsi que la possibilité de faire des simulations ayant pour objectif de faire des études d’optimisation"
+                        ],
+                        "postes": [
+                            {
+                                "nom": "Dev Lead / Tech Lead",
+                                "taille": 4,
+                                "roles": [
+                                    "Gestion et recrutement des développeurs",
+                                    "Analyse des besoins et des spécifications fonctionnelles",
+                                    "Organisation des releases (découpage, définition des besoins en ressources)",
+                                    "Gestion du reporting des coûts",
+                                    "Revue du code, test fonctionnels",
+                                    "Migration de .Net Framework 4.5 vers .Net5",
+                                    "Rédaction de la documentation technique",
+                                    "Support technique et développement de fonctionnalités complexes",
+                                    "Gestion des pipelines Azure DevOps"
+                                ],
+                                "techniques": [
+                                    "C#, Angular2+, .Net5, AngularJs",
+                                    "Entity Framework, Sql Server, Windows Server, GeoServer",
+                                    "HighChart, OpenLayer",
+                                    "Azure DevOps, Git, Moq"
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        "nom": "Aquaviz",
+                        "debut": "2017-06-01",
+                        "fin": "2018-08-01",
+                        "description": "Aquaviz est une librarie de composants UI et de modules utilisée par l'ensemble des logiciels Suez Smart Solutions permettant de gérer les données liées à la surveillance de l’environnement.",
+                        "desc": [
+                        ],
+                        "postes": [
+                            {
+                                "nom": "Dev Lead / Développeur Front End",
+                                "taille": 6,
+                                "roles": [
+                                    "Analyse des besoins et des spécifications fonctionnelles",
+                                    "Organisation des releases (découpage, définition des besoins en ressources)",
+                                    "Suivis de l’avancement du projet et des coûts",
+                                    "Revue du code, test fonctionnels",
+                                    "Développement de fonctionnalités (Form, Chart, Dashboard)",
+                                    "Rédaction de la documentation technique",
+                                    "Support technique"
+                                ],
+                                "techniques": [
+                                    "Angular 2+, HightCharts, Bootstrap, ngx-bootstrap, ag-grid",
+                                    "Visual Studio Code"
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        "nom": "Aquadvanced WaterNetwork Distribution",
+                        "debut": "2013-06-01",
+                        "fin": "2017-06-01",
+                        "description": "Aquadvanced est une solution de suivi et d'optimisation du fonctionnement des réseaux d'eau potable. La solution est composée par :",
+                        "desc": [
+                            "Un back office qui permet la collecte des données, ainsi que les calculs d'indicateurs et la détection de fuite à l'aide de modèles hydrauliques",
                             "Un front office qui offre une vision cartographique du réseau de distribution, des indicateurs et de la localisation des fuites"
                         ],
-                        "hiddentechs": "Subversion Visual Studio Anglais EntityFramework JavaScript",
+                        "postes": [
+                            {
+                                "nom": "Ingénieur développement / Responsable de maintenance",
+                                "taille": 6,
+                                "roles": [
+                                    "Analyse, conception et développement de la partie back office",
+                                    "Coordination technique avec l'équipe front office en Espagne",
+                                    "Analyse et qualification des anomalies",
+                                    "Assurer la cohérence entre différents besoins fonctionnels",
+                                    "Gestion de release et test d'intégration",
+                                    "Support technique",
+                                ],
+                                "techniques": [
+                                    "C#, WCF, TypeScript, AngularJs",
+                                    "Entity Framework 6, Sql Server, Windows Server, GeoServer",
+                                    "Visual Studio, TFS, Moq, CodeContracts"
+                                ]
+                            }
+                        ]
                     },
                     {
                         "nom": "Em2s",
                         "debut": "2011-02-01",
-                        "description": "Em2s est un framework métier modulaire mettant à disposition un moteur de calcul d'indicateurs, de gestion d’événements, d'authentification à destination des applications internes, telle que Aquadvanced, et externes.",
-                        "hiddentechs": "Subversion Netbeans JavaScript"
-                    }
-                ],
-                "postes": [
-                    {
-                        "nom": "Responsable de la maintenance applicative",
-                        "description": "Gestion de la maintenace corrective et réalisation des tests d'intégration sur les nouvelles fonctionnalités des applications Aquadvanced et Em2s",
-                        "debut": "2014-06-01",
-                        "roles": [
-                            "Analyse et qualification des anomalies",
-                            "Planification et suivi de la résolution des anomalies ",
-                            "Assurer la cohérence entre différents besoins fonctionnels",
-                            "Réalisation des tests d'intégration"
-                        ],
-                        "hiddentechs": "Subversion Visual Studio Anglais EntityFramework JavaScript",
-                    },
-                    {
-                        "nom": "Ingénieur développement sur l'application Aquadvanced",
-                        "debut": "2013-06-01",
-                        "taille": 6,
-                        "roles": [
-                            "Analyse, conception et développement de la partie back office",
-                            "Coordination technique avec l'équipe front office en Espagne",
-                            "Gestion de release et test d'intégration",
-                            "Support technique"
-                        ],
-                        "techniques": [
-                            "C#, WCF, TypeScript, AngularJs",
-                            "Entity Framework 6, MS Sql Server, Windows Server 2012, GeoServer",
-                            "Visual Studio, TFS, Moq, CodeContracts"
-                        ],
-                        "hiddentechs": "Subversion Visual Studio Anglais EntityFramework JavaScript",
-                    },
-                    {
-                        "nom": "Ingénieur développement sur framework métier Em2s",
-                        "debut": "2011-02-01",
-                        "taille": 4,
-                        "roles": [
-                            "Analyse, conception et développement",
-                            "Gestion de release et test d'intégration",
-                            "Support technique"
-                        ],
-                        "techniques": [
-                            "C#, WCF",
-                            "Entity Framework 6, MS Sql Server, Windows Server 2012",
-                            "Visual Studio, TFS, Moq, CodeContracts"
-                        ],
-                        "hiddentechs": "Subversion Visual Studio Anglais EntityFramework JavaScript",
+                        "fin": "2017-06-01",
+                        "description": "Em2s est un framework métier modulaire mettant à disposition un moteur de calcul d'indicateurs, de gestion d’événements, d'authentification à destination des applications internes.",
+                        "postes": [
+                            {
+                                "nom": "Ingénieur développement / Responsable de maintenance",
+                                "taille": 4,
+                                "roles": [
+                                    "Analyse, conception et développement des fonctionnalités",
+                                    "Analyse, qualification et suivi des anomalies",
+                                    "Assurer la cohérence entre différents besoins fonctionnels",
+                                    "Gestion de release et test d'intégration",
+                                    "Support technique"
+                                ],
+                                "techniques": [
+                                    "C#, WCF",
+                                    "Entity Framework 6, Sql Server, Windows Server",
+                                    "Visual Studio, TFS, Moq, CodeContracts"
+                                ]
+                            }
+                        ]
+
                     }
                 ]
             },
